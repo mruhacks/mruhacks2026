471cb377939edc13b49d833338ed28a7e3ca115e
Thomas Kapocsi <thomas@kapocsi.ca>
2025-10-19 00:11:37 -0600

    add basic middleware

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { headers } from "next/headers";
+import { auth } from "./utils/auth";
+
+export async function middleware(request: NextRequest) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/auth", request.url));
+  }
+  return NextResponse.next();
+}
+export const config = {
+  runtime: "nodejs",
+  matcher: ["/dashboard"], // Apply middleware to specific routes
+};
