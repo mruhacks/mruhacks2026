36eaf7472c1c1a98e5170a50e55789e14585cc96
sssk9034 <seilakennedy.y@gmail.com>
2026-02-21 00:36:36 -0700

    edit email address anchor and tailwind

--- src/components/contact-us.tsx
+++ src/components/contact-us.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 /**
  * ContactUs
  *
@@ -7,23 +9,20 @@
  */
 export function ContactUs() {
   return (
-    <section className='w-full bg-black px-4 py-[20px] sm:px-6 lg:px-16'>
+    <section className='w-full bg-black px-4 py-5 sm:px-6 lg:px-16'>
       <div className='max-w-2xl'>
         {/* title */}
-        <h2 className='mb-4 text-left text-[24px] font-semibold tracking-tight text-white'>
+        <h2 className='mb-4 text-left text-2xl font-semibold tracking-tight text-white'>
           Reach Out to Us
         </h2>
 
         {/* email */}
-        <p className='mb-6 text-left text-[16px] text-white'>
-          {' '}
-          <a
-            href='mailto:mruhacks@gmail.com'
-            className='underline transition-colors hover:text-gray-300'
-          >
-            mruhacks@gmail.com
-          </a>
-        </p>
+        <Link
+          href='mailto:hello@mruhacks.ca'
+          className='text-white underline transition-colors hover:text-gray-300'
+        >
+          hello@mruhacks.ca
+        </Link>
       </div>
     </section>
   );
