e5eedd7b57b0bc369b50a787bc5c85e0b4a7196c
sssk9034 <seilakennedy.y@gmail.com>
2026-02-25 12:58:22 -0700

    change contents.ts directory

---
+++ src/components/faq-section.tsx
@@ -0,0 +1,5 @@
+import { ChevronDownIcon } from "lucide-react";
+import * as React from 'react';
+
+import { FAQs } from "../content";
+import { Description, MainTitle, PreTitle } from "./ui/typography";
