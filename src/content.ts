f10305bf25c884b099531ae4f1ed5a712b2e384e
sssk9034 <seilakennedy.y@gmail.com>
2026-02-25 13:42:59 -0700

    move contents.ts file to src directory

---
+++ src/content.ts
@@ -0,0 +1,7 @@
+export const INTRO_BLURB: string = `
+MRUHacks is Mount Royal University’s largest hackathon, bringing
+students together for an immersive three-day experience focused on
+hands-on building, collaboration, and innovation. Hosted at the
+Riddell Library and Learning Centre, the event welcomes designers,
+developers, and tech enthusiasts of all skill levels.
+`;
