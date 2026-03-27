ce5e1e012cf32095646ee0b84e48d781881014ec
sunny <aparm539@mtroyal.ca>
2026-03-27 16:51:07 -0600

    revert change to vitest.config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vitest/config';
 
-const alias = {
-  '@': new URL('./src', import.meta.url).pathname,
-};
-
 export default defineConfig({
   test: {
     globals: true,
-    setupFiles: ['./src/tests/setup.ts'],
     environment: 'node',
-    alias,
+    alias: {
+      '@': new URL('./src', import.meta.url).pathname,
+    },
+    setupFiles: ['./src/tests/setup.ts'],
   },
 });
